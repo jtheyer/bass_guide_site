19b6a28577367efedc1d4831c6105c1c1c2f632c
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2022-03-11 22:56:08 -0500

    Swiper cube working properly

--- src/app/harbor/harbor.page.ts
+++ src/app/harbor/harbor.page.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import Swiper, { SwiperOptions, Pagination } from 'swiper';
+import { AfterContentChecked, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+import Swiper, { SwiperOptions, Autoplay, Pagination, EffectCube } from 'swiper';
+import { IonicSlides } from '@ionic/angular';
+
+Swiper.use([Autoplay, Pagination, EffectCube, IonicSlides]);
 
 @Component({
   selector: 'app-harbor',
   templateUrl: './harbor.page.html',
   styleUrls: ['./harbor.page.scss'],
 })
-export class HarborPage implements OnInit {
+export class HarborPage implements AfterContentChecked {
+  @ViewChild('swiper') swiper: SwiperComponent;
 
   title = 'Welcome';
 
   constructor() { }
 
   public swiperConfig: SwiperOptions = {
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true,
+    },
     pagination: true,
-    observer: true,
-    observeParents: true
+    // observer: true,
+    // observeParents: true,
+    // slidesPerView: 3,
+    slidesPerView: 'auto',
+    effect: 'cube',
+    loop: true
   };
 
-  ngOnInit() {
-    Swiper.use([Pagination]);
+  ngAfterContentChecked(): void {
+    if (this.swiper){
+      this.swiper.updateSwiper({});
+    }
   }
 
 }
