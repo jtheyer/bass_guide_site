2488edfc4f9d07e90fc716c47e2c7398d4dcf35e
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2022-03-15 23:40:28 -0400

    update "about" page

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -6,15 +6,12 @@ import { IonicModule } from '@ionic/angular';
 import { AboutPageRoutingModule } from './about-routing.module';
 import { AboutPage } from './about.page';
 
-import { SwiperModule } from 'swiper/angular';
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     AboutPageRoutingModule,
-    SwiperModule
   ],
   declarations: [AboutPage]
 })
