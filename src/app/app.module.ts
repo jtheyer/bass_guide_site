1ea6cad1738b8c2baf045f3cd595e3db4e79c3a0
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2022-03-17 19:21:49 -0400

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [/*are to make directives (including components and pipes) 
@@ -19,6 +23,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [{/*are to make services and values known to DI (dependency injection).
      They are added to the root scope and they are injected to other 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { FIREBASE_ENV } from '../../database/firebase_env';
 
 export const environment = {
+  firebase: FIREBASE_ENV,
   production: false
 };
 
